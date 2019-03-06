c03df5237a595c66399a568e30949642b4480e4a
tknr <geishagirlskoume@gmail.com>
2019-03-07 01:29:57 +0900

    2019年  3月  7日 木曜日 01:29:57 JST

--- install_ffmpeg_amd64.sh
+++ install_ffmpeg_amd64.sh
@@ -3,7 +3,7 @@ echo "this command must be executed in root user."
 wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz || exit 1
 tar xvf ffmpeg-git-amd64-static.tar.xz || exit 1
 rm ffmpeg-git-amd64-static.tar.xz
-mv ffmpeg-* ffmpeg-giti || exit 1
+mv ffmpeg-* ffmpeg-git || exit 1
 sudo cp -f ffmpeg-git/ff* /usr/local/bin/ || exit 1
 sudo cp -f ffmpeg-git/qt-faststart /usr/local/bin/ || exit 1
 sudo cp -Rf ffmpeg-git/model /usr/local/share/ffmpeg/ || exit 1
