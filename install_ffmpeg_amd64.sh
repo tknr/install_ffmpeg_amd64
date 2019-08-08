fbf323e2ae3411e2463143e9b113efed275cfd9d
tknr <geishagirlskoume@gmail.com>
2019-08-08 22:26:28 +0900

    2019年  8月  8日 木曜日 22:26:27 JST

--- install_ffmpeg_amd64.sh
+++ install_ffmpeg_amd64.sh
@@ -9,8 +9,12 @@ tar xvf ffmpeg-git-amd64-static.tar.xz || exit 1
 rm ffmpeg-git-amd64-static.tar.xz
 mv ffmpeg-* ffmpeg-git || exit 1
 cp -f ffmpeg-git/ff* /usr/local/bin/ || exit 1
+chmod 755 /usr/local/bin/ff* || exit 1
 cp -f ffmpeg-git/qt-faststart /usr/local/bin/ || exit 1
+chmod 755 /usr/local/bin/qt-faststart || exit 1
+mkdir -p /usr/local/share/ffmpeg || exit 1
 cp -Rf ffmpeg-git/model /usr/local/share/ffmpeg/ || exit 1
+chmod -R 755 /usr/local/share/ffmpeg || exit 1
 rm -Rf ffmpeg-git
 
 
